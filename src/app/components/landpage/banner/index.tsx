c854e6b5897ff2f31c6a673255cf2e1cc3daea36
cordy <mglgain@gmail.com>
2025-11-28 00:09:20 +0800

    feat: enhance Banner component with improved layout and content; update metadata for consistency

--- src/app/components/landpage/banner/index.tsx
+++ src/app/components/landpage/banner/index.tsx
@@ -4,14 +4,15 @@ export default function Banner() {
 
     return(
         <section className={`${styles.container} display_flex_center`}>
-            <div className={`${styles.left_side} display_flex_center`}>
-                <h1>LACO AI</h1>
-                <p>
-
-                </p>
-            </div>
-            <div className={styles.right_side}>
-                
+            <div className={`${styles.wrapper} display_flex_center`}>
+                <section className={styles.left_side}>
+                    <h1>LACO Artificial Intelligence</h1>
+                    <p>LACO AI is an intelligent PDF summarization and analysis system enhanced by Gemeni AI technologies.</p>
+                    <p>It is designed to support academic workflows by automatically extracting and condensing essential information from lengthy documents, enabling faster comprehension and improving research efficiency.</p>
+                </section>
+                <section className={styles.right_side}>
+                    
+                </section>
             </div>
         </section>
     );
