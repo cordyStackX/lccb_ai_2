9893427300d9206413aff4d802aec40e98488c82
cordystackx <cordovamarcgiestinlouis@gmail.com>
2026-02-22 16:17:43 +0800

    feat: implement JWT verification and redirect logic in Chat component

--- src/app/(pages)/chat/page.tsx
+++ src/app/(pages)/chat/page.tsx
@@ -1,8 +1,19 @@
 "use client";
 import { Fetch_to } from "@/utilities";
 import api_link from "@/config/conf/json_config/fetch_url.json";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Chat() {
+    const router = useRouter();
+
+     useEffect(() => {
+        async function check() {
+            const response = await Fetch_to(api_link.jwt.verify);
+            if (!response.success) return router.push("/");
+        }
+        check();
+    }, []);
 
     return(
         <section style={{ width: "100vw", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center", flexFlow: "column" }}>
@@ -13,6 +24,7 @@ export default function Chat() {
             >Check the Github Repo</a>
             <a onClick={async () => {
                 await Fetch_to(api_link.jwt.deauth);
+                router.push("/");
             }} style={{ color: "red", fontWeight: "bolder", cursor: "pointer" }}> Log Out </a>
         </section>
     );
