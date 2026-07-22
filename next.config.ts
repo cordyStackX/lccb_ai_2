fe90afb4f2c7fd550a131837c88440f133cf5146
cordystackx <mglgain@gmail.com>
2026-07-23 07:45:18 +0800

    refactor(next.config): remove unused images remotePatterns configuration

--- next.config.ts
+++ next.config.ts
@@ -3,17 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     allowedDevOrigins: [
       ""
-    ],
-    images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "8000",
-        pathname: "/storage/v1/object/public/**",
-      },
-    ],
-  },
+    ]
 };
 
 export default nextConfig;
